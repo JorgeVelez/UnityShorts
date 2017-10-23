740f6142d7ca896991b489826b3ff8d55c80f304
JorgeVelez <jorgeluisvelez@gmail.com>
2017-10-23 12:01:31 -0400

    todo

---
+++ template/Assets/Regions.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Regions : MonoBehaviour
+{
+	#region Public Fields
+	#endregion
+
+	#region Unity Methods
+	void Start()
+	{
+		
+	}
+
+	void Update()
+	{
+	}
+	#endregion
+
+	#region Private Methods
+	#endregion
+}
